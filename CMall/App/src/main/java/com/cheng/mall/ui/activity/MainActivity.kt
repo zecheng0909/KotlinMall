5637ff2f8e0ee4f48fafbfbf697742d10e02efb3
zecheng0909 <514118702@qq.com>
2018-01-01 19:32:48 +0800

    完成了项目初始化. 1-6

---
+++ CMall/App/src/main/java/com/cheng/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cheng.mall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cheng.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
