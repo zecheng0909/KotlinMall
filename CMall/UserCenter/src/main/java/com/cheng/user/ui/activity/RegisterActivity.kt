f0c797be0182ceed4dd7c378dcd2048d140a7c74
zecheng0909 <514118702@qq.com>
2018-01-01 21:42:59 +0800

    初始化了用户模块

---
+++ CMall/UserCenter/src/main/java/com/cheng/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.cheng.user.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cheng.user.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
